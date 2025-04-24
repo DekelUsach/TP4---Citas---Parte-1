7bc0bc3e98da027ee6e254a082ed69eea91b77cc
DEKEL USACH <48386383@ort>
2025-04-24 08:48:57 -0300

    started design

---
+++ src/Formulario.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Input from './Input'
+
+export default function Formulario() {
+  return (
+<div className="card text-center">
+  <div className="card-header">
+    Listado
+  </div>    
+  <div className="card-body">
+  <Input />
+  </div>
+</div>
+  )
+}
---
+++ src/Input.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+
+export default function Input() {
+
+  return (
+        <input className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example"></input>
+  )
+}
