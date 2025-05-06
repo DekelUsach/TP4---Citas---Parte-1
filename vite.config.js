80bf3db4862e60e114bcda5d26f3732656a07f6a
DEKEL USACH <48386383@ort>
2025-05-06 09:02:30 -0300

    Prepared build for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TP4---Citas---Parte-1/',
   plugins: [react()],
 })
