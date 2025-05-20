727999e557195c1bd4be7bd9c860c991e470e724
DEKEL USACH <48386383@ort>
2025-05-20 09:47:57 -0300

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TP4---Citas---Parte-1/',
+  base: '/TP4-Citas-partes-unidas/',
   plugins: [react()],
 })
